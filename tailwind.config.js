f08283dc9b3e07cc3759321564883d2f8e0b5c59
Iqbal Hasnan <iqbal.hasnan@gmail.com>
2021-10-27 12:25:22 +0800

    CG-4: initial tailwinds and home page

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: ["production", "staging"].includes(process.env.NODE_ENV),
+    content: [
+      './app/views/**/*.html.erb',
+      './app/helpers/**/*.rb',
+      './app/javascript/**/*.js',
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
+}
